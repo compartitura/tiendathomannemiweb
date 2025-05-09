ee7094b4694adf5b3d127a00bd08d40d313c21ad
compartitura <compartitura@gmail.com>
2025-05-10 00:57:16 +0200

    Corrijo imports de CSS y añado @fontsource/inter

--- pages/app.js
+++
@@ -1,6 +0,0 @@
-// pages/_app.js
-import '@fontsource/inter/variable.css';
-import '@/styles/globals.css';
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
